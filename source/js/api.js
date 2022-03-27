c71c235dc3aeb3e443fc7e1f02e32cf4cb7f3de8
Yana Rozova <yana.rozova@mail.ru>
2022-03-27 14:22:10 +0300

    fix code style

--- source/js/api.js
+++ source/js/api.js
@@ -3,9 +3,9 @@ import data from './../json/data.json';
 const GET_LINK = 'https://23.javascript.pages.academy/keksobooking/data404';
 
 const getData = (onSuccess, onError) => {
-fetch(GET_LINK)
+  fetch(GET_LINK)
     .then((response) => {
-      return response.ok? response.json(): data;
+      return response.ok ? response.json() : data;
     })
     .then((flats) => onSuccess(flats))
     .catch(() => onError());
